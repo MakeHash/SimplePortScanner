f496232e1ee4fc4a64e61bca71164c8297c48e3b
MXMLN <137910747+MakeHash@users.noreply.github.com>
2023-08-30 20:52:26 +0300

    Update prtScn.h

--- PortScan(LINUX)/prtScn.h
+++ PortScan(LINUX)/prtScn.h
@@ -1,13 +1,1 @@
-#include <arpa/inet.h>
-#include <stdio.h>
-#include <sys/socket.h>
-#include <stdlib.h>
-#include <unistd.h>
 
-#include <string.h>
-
-#include <vector>
-#include <algorithm>
-using std::vector;
-
-static vector<int> open_ports;
